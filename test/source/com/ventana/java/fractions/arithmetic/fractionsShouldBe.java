910d8cb9794276ae1668b907979e472d7c7602cb
wpannell <wil.pannell@gmail.com>
2013-08-28 08:48:52 -0700

    refactored test class for legibility

--- test/source/com/ventana/java/fractions/arithmetic/fractionsShouldBe.java
+++ test/source/com/ventana/java/fractions/arithmetic/fractionsShouldBe.java
@@ -6,13 +6,13 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-public class fractionsShould {
+public class fractionsShouldBe {
   private int fractions(int... nums) {
     return 0;
   }
 
   @Test
-  public void addTwoZeroesAndGetZero() {
+  public void zeroWhenTwoZeroFractionsAreAdded() {
     assertThat(fractions(0,9,0,10), is(equalTo(0)));
   }
 }
