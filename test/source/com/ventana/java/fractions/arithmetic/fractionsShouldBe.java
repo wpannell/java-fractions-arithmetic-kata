7433f9b5ddff389060792a9619801626aed7c09e
wpannell <wil.pannell@gmail.com>
2013-08-28 08:55:15 -0700

    refactored class for usability

--- test/source/com/ventana/java/fractions/arithmetic/fractionsShouldBe.java
+++ test/source/com/ventana/java/fractions/arithmetic/fractionsShouldBe.java
@@ -7,12 +7,14 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class fractionsShouldBe {
-  private int fractions(int... nums) {
-    return 0;
+  private static final int[] ZERO = new int[]{0, 1};
+
+  private int[] fractions(int... nums) {
+    return ZERO;
   }
 
   @Test
   public void zeroWhenTwoZeroFractionsAreAdded() {
-    assertThat(fractions(0,9,0,10), is(equalTo(0)));
+    assertThat(fractions(0,9,0,10), is(ZERO));
   }
 }
