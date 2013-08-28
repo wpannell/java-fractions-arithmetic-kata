f910c391ffaaea0ca900e6af003bbeb72ff8d034
wpannell <wil.pannell@gmail.com>
2013-08-28 08:46:41 -0700

    refactored test:  addZeroesAndGetZero

--- test/source/com/ventana/java/fractions/arithmetic/fractionsShould.java
+++ test/source/com/ventana/java/fractions/arithmetic/fractionsShould.java
@@ -7,12 +7,12 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class fractionsShould {
-  @Test
-  public void addZeroesAndGetZero() {
-    assertThat(fractions(0,9,0,10), is(equalTo(0)));
-  }
-
   private int fractions(int... nums) {
     return 0;
+  }
+
+  @Test
+  public void addTwoZeroesAndGetZero() {
+    assertThat(fractions(0,9,0,10), is(equalTo(0)));
   }
 }
