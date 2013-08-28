110b27bcc588a0f62385807a941ba021fa337a48
wpannell <wil.pannell@gmail.com>
2013-08-28 08:45:17 -0700

    todo list; passing test:  addZeroesAndGetZero

--- test/source/com/ventana/java/fractions/arithmetic/fractionsShould.java
+++ test/source/com/ventana/java/fractions/arithmetic/fractionsShould.java
@@ -2,8 +2,17 @@ package com.ventana.java.fractions.arithmetic;
 
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class fractionsShould {
   @Test
-  public void showUnitTestCanRun() {
+  public void addZeroesAndGetZero() {
+    assertThat(fractions(0,9,0,10), is(equalTo(0)));
+  }
+
+  private int fractions(int... nums) {
+    return 0;
   }
 }
