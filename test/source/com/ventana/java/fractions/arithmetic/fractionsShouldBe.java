c3d1ed3f0b847e15ef6d17cebd0809ed9d52a832
wpannell <wil.pannell@gmail.com>
2013-08-28 08:57:07 -0700

    failing test:  theFractionWhenZeroIsAddedToAnyFraction

--- test/source/com/ventana/java/fractions/arithmetic/fractionsShouldBe.java
+++ test/source/com/ventana/java/fractions/arithmetic/fractionsShouldBe.java
@@ -17,4 +17,10 @@ public class fractionsShouldBe {
   public void zeroWhenTwoZeroFractionsAreAdded() {
     assertThat(fractions(0,9,0,10), is(ZERO));
   }
+
+  @Test
+  public void theFractionWhenZeroIsAddedToAnyFraction() {
+    assertThat(fractions(0,100,1,99), is(equalTo(new int[]{1,99})));
+    assertThat(fractions(1,777, 0,396), is(equalTo(new int[]{1,777})));
+  }
 }
