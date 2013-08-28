de3bb1f469cae5cdcee8df2c84c3a6988102bc24
wpannell <wil.pannell@gmail.com>
2013-08-28 13:26:17 -0700

    restored test methods inadvertently removed

--- test/source/com/ventana/java/fractions/arithmetic/FractionAdditionShouldBe.java
+++ test/source/com/ventana/java/fractions/arithmetic/FractionAdditionShouldBe.java
@@ -2,8 +2,35 @@ package com.ventana.java.fractions.arithmetic;
 
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class FractionAdditionShouldBe {
   @Test
   public void theFractionWhenZeroIsAddedToAnyFraction() {
+  }
+
+  @Test
+  public void theSumOfTheNumeratorsWhenTheDenominatorsAreEqual() {
+    assertThat("4/7 + 11/7)", TestData.fourOver7.plus(TestData.elevenOver7),
+        is(TestData.fifteenOver7));
+  }
+
+  @Test
+  public void reducedWhenTheSumHasAGreatestCommonDivisor() {
+    assertThat("10/20 + 30/20)", TestData.tenOver20.plus(TestData.thirtyOver20),
+        is(TestData.twoOver1));
+  }
+
+  @Test
+  public void crossMultipliedWhenTheDenominatorsAreUnequal() {
+    assertThat("4/7 + 11/13)", TestData.fourOver7.plus(TestData.elevenOver13),
+        is(TestData.oneTwentyNineOver91));
+
+    assertThat("5/10 + 10/10)", TestData.fiveOver10.plus(TestData.tenOver10),
+        is(TestData.threeOver2));
+
+    assertThat("2/5 + 2/10)", TestData.twoOver5.plus(TestData.twoOver10),
+        is(TestData.threeOver5));
   }
 }
