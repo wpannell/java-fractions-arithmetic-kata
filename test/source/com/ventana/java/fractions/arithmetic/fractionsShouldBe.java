8a61c9a3b835ac28c35647c11a4531d63ae75a90
wpannell <wil.pannell@gmail.com>
2013-08-28 09:58:01 -0700

    added more assertions for clarity:  crossMultipliedWhenTheDenominatorsAreUnequal

--- test/source/com/ventana/java/fractions/arithmetic/fractionsShouldBe.java
+++ test/source/com/ventana/java/fractions/arithmetic/fractionsShouldBe.java
@@ -16,6 +16,7 @@ public class fractionsShouldBe {
     if(numerator2 == 0) return reduce(numerator1, denominator1);
     if(denominator1 == denominator2)
       return reduce(numerator1 + numerator2, denominator1);
+
     return reduce((numerator1*denominator2) + (numerator2*denominator1),
         denominator1 * denominator2);
   }
@@ -34,23 +35,34 @@ public class fractionsShouldBe {
 
   @Test
   public void theFractionWhenZeroIsAddedToAnyFraction() {
-    assertThat("fractions(0,100,1,99)", fractions(0,100,1,99), is(equalTo(new int[]{1,99})));
-    assertThat("fractions(1,777,0,396)", fractions(1,777,0,396), is(equalTo(new int[]{1,777})));
+    assertThat("fractions(0,100,1,99)",
+        fractions(0,100,1,99), is(equalTo(new int[]{1,99})));
+
+    assertThat("fractions(1,777,0,396)",
+        fractions(1,777,0,396), is(equalTo(new int[]{1,777})));
   }
 
   @Test
   public void theSumOfTheNumeratorsWhenTheDenominatorsAreEqual() {
-    assertThat("fractions(4,7,11,7)", fractions(4,7,11,7), is(equalTo(new int[]{15,7})));
+    assertThat("fractions(4,7,11,7)",
+        fractions(4,7,11,7), is(equalTo(new int[]{15,7})));
   }
 
   @Test
   public void reducedWhenTheSumHasAGreatestCommonDivisor() {
-    assertThat("fractions(10,20,30,20)", fractions(10,20,30,20), is(equalTo(new int[]{2,1})));
+    assertThat("fractions(10,20,30,20)",
+        fractions(10,20,30,20), is(equalTo(new int[]{2,1})));
   }
 
   @Test
   public void crossMultipliedWhenTheDenominatorsAreUnequal() {
     assertThat("fractions(4,7,11,13)",
         fractions(4,7,11,13), is(equalTo(new int[]{129,91})));
+
+    assertThat("fractions(5,10,10,10)",
+        fractions(5,10,10,10), is(equalTo(new int[]{3,2})));
+
+    assertThat("fractions(2,5,2,10)",
+        fractions(2,5,2,10), is(equalTo(new int[]{3,5})));
   }
 }
