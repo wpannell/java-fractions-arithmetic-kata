62716dcc23e42c8bd9d4f02f890d21a5de5aec79
wpannell <wil.pannell@gmail.com>
2013-08-29 07:21:35 -0700

    improved test legibility

--- test/source/com/ventana/java/fractions/arithmetic/FractionMultiplicationShouldBe.java
+++ test/source/com/ventana/java/fractions/arithmetic/FractionMultiplicationShouldBe.java
@@ -7,17 +7,14 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class FractionMultiplicationShouldBe {
-  private static int n = 13;
-  private static int m = 7;
-  private static int i = 3;
   private static int j = 11;
 
   @Test
   public void zeroWhenAnyFractionIsZero() {
-    assertThat("0/n * 0/m", new Fraction(0,n).times(new Fraction(0,m)),
-        is(equalTo(new Fraction(0,m))));
+    assertThat("0/13 * 0/7", new Fraction(0, 13).times(new Fraction(0, 7)),
+        is(equalTo(Fraction.ZERO)));
 
-    assertThat("0/n * i/m", new Fraction(0,n).times(new Fraction(i,m)),
-        is(equalTo(new Fraction(0,m))));
+    assertThat("0/13 * 3/7", new Fraction(0, 13).times(new Fraction(3, 7)),
+        is(equalTo(Fraction.ZERO)));
   }
 }
