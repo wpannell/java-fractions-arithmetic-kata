5e4ac7e76eb73c996def672164a0bc81ed67d309
wpannell <wil.pannell@gmail.com>
2013-08-29 07:28:31 -0700

    failing test: theFractionWhenItIsMultipliedByIdentity

--- test/source/com/ventana/java/fractions/arithmetic/FractionMultiplicationShouldBe.java
+++ test/source/com/ventana/java/fractions/arithmetic/FractionMultiplicationShouldBe.java
@@ -17,4 +17,10 @@ public class FractionMultiplicationShouldBe {
     assertThat("0/13 * 3/7", new Fraction(0, 13).times(new Fraction(3, 7)),
         is(equalTo(Fraction.ZERO)));
   }
+
+  @Test
+  public void theFractionWhenItIsMultipliedByIdentity() {
+    assertThat("3/13 * 7/7", new Fraction(3, 13).times(new Fraction(7, 7)),
+        is(equalTo(new Fraction(3, 13))));
+  }
 }
